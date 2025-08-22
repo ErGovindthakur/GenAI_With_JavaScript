7c6d44b6ebaf5b16acb36f4908d8a6a3c86cba34
ErGovindthakur <ergovindthakur@gmail.com>
2025-08-22 07:56:56 +0530

    implement tool calls

--- Chapter_03/ToolCalling.js
+++ Chapter_03/ToolCalling.js
@@ -1,35 +1,86 @@
-import Groq from 'groq-sdk';
-import dotenv from 'dotenv';
+import Groq from "groq-sdk";
+import dotenv from "dotenv";
 
 dotenv.config();
 
-const groq = new Groq({apiKey:process.env.GROQ_API_KEY});
+const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
 
-let callTool = async() => {
-     try {
-          const tool = await groq.chat.completions.create({
-               model:"llama-3.3-70b-versatile",
-               temperature:0,
+let callTool = async () => {
+  try {
+    const tool = await groq.chat.completions.create({
+      model: "llama-3.3-70b-versatile",
+      temperature: 0,
 
-               messages:[
-                    {
-                         role:'system',
-                         content:`You are a smart personal assistant. 
-                         - always reply in simple and easy words.
-                         - keep the answer concise (preferably one sentence).
-                         If unsure, say "I am not sure" instead of guessing.`
-                    },
-                    {
-                         role:'user',
-                         content:'Who is Virat Kohli ?'
-                    }
-               ]
-          })
+      messages: [
+        {
+          role: "system",
+          // content: `You are a smart personal assistant.
+          //                - always reply in simple and easy words.
+          //                - keep the answer concise (preferably one sentence).
+          //                If unsure, say "I am not sure" instead of guessing.`,
+          content: `you are a smart personal assistant, who answers the asked question.
+          You have access to following tools:
+          1. searchWeb({query}:{query: string}) //Search the latest information and realtime data on the internet. `,
+        },
+        {
+          role: "user",
+          // content: "Who is Virat Kohli ?",
+          content:"When was iphone 16 launched ?"
+        },
+      ],
+      tools: [
+        {
+          type: "function",
+          function: {
+            name: "webSearch",
+            description:
+              "Search the latest information and realtime data on the internet.",
+            parameters: {
+              type: "object",
+              properties: {
+                query: {
+                  type: "string",
+                  description: "The search query to perform search on.",
+                },
+              },
+              required: ["query"],
+            },
+          },
+        },
+      ],
+      tool_choice: "auto",
+    });
 
-          console.log(tool.choices[0].message.content)
-     } catch (error) {
-          console.log(error.message)
+    const toolCalls = tool.choices[0].message.tool_calls;
+
+    if(!toolCalls){
+     console.log(`Assistant : ${tool.choices[0].message.content}`);
+     return;
+    }
+
+    for(const tools of toolCalls){
+     console.log(`tool : `,tools)
+     const functionName = tools.function.name;
+     const functionArgs = tools.function.arguments;
+
+     if(functionName === 'webSearch'){
+          const toolResult = await webSearch(JSON.parse(functionArgs));
+          console.log(toolResult)
      }
-}
+    }
+
+
+//     console.log(tool.choices[0].message.tool_calls);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
 callTool();
+
+async function webSearch({ query }) {
+  // Here we will do tavily api call
+
+  console.log("Calling webSearch...");
+  return `Iphone 16 was launched on 20 september 2024.`;
+}
